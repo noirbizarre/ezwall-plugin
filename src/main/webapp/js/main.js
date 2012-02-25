a8b2d4e16b23328759bad866f02e326bbe7ff499
Axel Haustant <noirbizarre@gmail.com>
2012-02-25 13:10:46 +0100

    Added main.js and removed inline javascript.

---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,22 @@
+console.log('EZWall starting...');
+require.config({
+	paths: {
+		jquery: 'libs/jquery-1.7.1.min',
+		jqueryui: 'libs/jquery-ui-1.8.17.min',
+		jquerytmpl: 'libs/jquery.tmpl.min',
+		underscore: 'libs/underscore-amd-min',
+		backbone: 'libs/backbone-0.9.1-amd-min',
+		md5: 'libs/spark-md5.amd'
+	}
+});
+
+require([
+	'order!jquery',
+	'order!underscore',
+	'order!backbone',
+	'order!jquerytmpl',
+	'order!jqueryui',
+	'order!app'
+], function($$, _, backbone, jqueryTmpl, jqueryUi, App){
+	App.start();
+});
